4c0ee56b3a533abffba23d666030cb4d7f5e1c74
Oleksii Koshelenko <oleksii_koshelenko@epam.com>
2024-05-30 16:42:42 +0300

    min func worked MySQL and PostgreSQL

---
+++ backups/postgresql/test_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO test_table (id, created_at, name) VALUES ('1', 'Test Name 1', '2024-05-30 13:40:36.365257');
+INSERT INTO test_table (id, created_at, name) VALUES ('2', 'Test Name 2', '2024-05-30 13:40:36.367817');
+INSERT INTO test_table (id, created_at, name) VALUES ('3', 'Test Name 3', '2024-05-30 13:40:36.368953');
